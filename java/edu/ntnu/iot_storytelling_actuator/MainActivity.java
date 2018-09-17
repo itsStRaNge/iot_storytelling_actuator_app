098aa29b38f0f6fd5bee861dfa9fb29905755cca
Lukas Bernhard <L.J.Bernhard@web.de>
2018-09-17 09:42:47 +0200

    init

---
+++ java/edu/ntnu/iot_storytelling_actuator/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.ntnu.iot_storytelling_actuator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
