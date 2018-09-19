f0a9d1cc8f84f58b92cf36e9d8738e19d7fa4f9c
Lukas Bernhard <L.J.Bernhard@web.de>
2018-09-19 10:57:47 +0200

    can download and display/play images/sounds

--- java/edu/ntnu/iot_storytelling_actuator/MainActivity.java
+++ java/edu/ntnu/iot_storytelling_actuator/MainActivity.java
@@ -4,17 +4,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,27 +49,68 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        try {
-            Intent i = getIntent();
-            String msg = i.getStringExtra("message");
-            displayMessage(msg);
-        }catch(NullPointerException e){
-            Log.d("Debug", "Empty Intent");
-        }
+        handleServerData();
     }
     protected void onNewIntent(Intent i) {
         super.onNewIntent(i);
-        try {
-            String msg = i.getStringExtra("message");
-            displayMessage(msg);
-        }catch(NullPointerException e){
-            Log.d("Debug", "Empty Intent, onNewIntent");
-        }
+        handleServerData();
     }
-    public void displayMessage(String msg){
-        TextView view = (TextView) findViewById(R.id.text_view1);
-        view.setText(msg);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.sound);
-        mp.start();
+
+    public void handleServerData() {
+        try {
+            Intent i = getIntent();
+            String json_str = i.getStringExtra("message");
+            JSONObject json = new JSONObject(json_str);
+
+            if(json.has("audio")) {
+                try {
+                    String url =json.getString("audio");
+                    MediaPlayer mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.setDataSource(url);
+                    mediaPlayer.prepare(); // might take long! (for buffering, etc)
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(json.has("image")) {
+                new ImageRequest().execute(json.getString("image"));
+            }
+
+
+        } catch (NullPointerException e) {
+            Log.e("Error", "NullPointerException");
+        } catch (JSONException e) {
+            Log.e("Error", "JSON Exception");
+        }
+
+    }
+
+    private class ImageRequest  extends AsyncTask<String, Integer, Bitmap> {
+
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                java.net.URL url = new java.net.URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                return BitmapFactory.decodeStream(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        protected void onPostExecute(Bitmap bit) {
+            if(bit != null)
+                ((ImageView) findViewById(R.id.image_view)).setImageBitmap(bit);
+        }
     }
 }
